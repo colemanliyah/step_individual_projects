546dfa48b3f4fecc06cbe7d32d0abbab586d4cef
liyahcoleman <colemanliyah@gmail.com>
2020-06-01 10:03:09 -0700

    Added form feature to html and added request functions in java to get the data from the server

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -23,25 +23,42 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/data")
+// @WebServlet("/data")
+@WebServlet("/comments")
 public class DataServlet extends HttpServlet {
 
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String saying = "Hello Again";
-    ArrayList<String> messages = new ArrayList<String>();
-    messages.add("message one");
-    messages.add("message two");
-    messages.add("message three");
-    String json = convertToJsonUsingGson(messages);
+//   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//     String saying = "Hello Again";
+//     ArrayList<String> messages = new ArrayList<String>();
+//     messages.add("message one");
+//     messages.add("message two");
+//     messages.add("message three");
+//     String json = convertToJsonUsingGson(messages);
 
-    response.setContentType("text/html;");
-    response.getWriter().println(json);
+//     response.setContentType("text/html;");
+//     response.getWriter().println(json);
+//   }
+
+//   private String convertToJsonUsingGson(ArrayList<String> mes) {
+//      Gson gson = new Gson();
+//     String json = gson.toJson(mes);
+//     return json;
+//   }
+
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      ArrayList<String> messages = new ArrayList<String>();
+      String text = getParameter(request, "comment", "");
+      messages.add(text);
+      response.setContentType("text/html;");
+      response.getWriter().println(messages);
   }
 
-  private String convertToJsonUsingGson(ArrayList<String> mes) {
-     Gson gson = new Gson();
-    String json = gson.toJson(mes);
-    return json;
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+      String value = request.getParameter(name);
+      if (value == null) {
+          return defaultValue;
+      }
+      return value;
   }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,12 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-console.log("reached here")
-
 function fetchfunction(){
     fetch("/data").then(response => response.json()).then((json) => {
         document.getElementById('testing').innerText = json;
-        console.log(json);
     });
 }
 
