e1a1acfac16781ff7460f6aee4ec8b6f02aad2d6
liyahcoleman <colemanliyah@gmail.com>
2020-06-09 23:58:08 +0000

    Added markers on the map and made the map more interactive by letting the user guess some of the places I have been

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -39,9 +39,79 @@ function deleteComment(key){
     });
 }
 
+let map=null;
 function createMap() {
-  const map = new google.maps.Map(
-      document.getElementById('map'),
-      {center: {lat: 37.422, lng: -122.084}, zoom: 8});
+    map = new google.maps.Map(
+    document.getElementById('map'),
+    {center: {lat: 37.422, lng: -122.084}, zoom: 4, mapTypeId: 'hybrid'});
+
+    let homeMarkerInfo = new google.maps.InfoWindow({
+        content: "<h6> Phoenix </h6>" + "</br>" + "<p>I have grown up in Arizona all my life. I lived in Virgina for three months when I was eight because my dad is from there but otherwise I have only lived in AZ</p>"
+    });
+
+    let homeMarker = new google.maps.Marker({
+        position: {lat: 34.049, lng: -111.094},
+        map: map,
+        title: "My Home State"
+    });
+
+    homeMarker.addListener('click', function() {
+        homeMarkerInfo.open(map, homeMarker);
+    });
+
+    let travelMarkerInfo = new google.maps.InfoWindow({
+        content: "<h1> Italy </h1> " + "</br>" + "<p> If I could go anywhere in the world, I would go to Italy. I have never been there before </p>"
+    })
+
+    let travelMarker = new google.maps.Marker({
+        position: {lat: 41.872, lng: 12.567},
+        map:map,
+    });
+
+    travelMarker.addListener('click', function(){
+        travelMarkerInfo.open(map, travelMarker);
+    });
+
+    firstTimeGoogleInfo = new google.maps.InfoWindow({
+        content: "<h6> Seattle </h6>" + "</br>" + "<p> My first experience being at Google was here in Seattle the summer of 2019!</p>"
+    });
+
+    let firstTimeGoogle = new google.maps.Marker({
+        position: {lat: 47.606, lng: -122.332},
+        map:map,
+    });
+
+    firstTimeGoogle.addListener('click', function(){
+        firstTimeGoogleInfo.open(map, firstTimeGoogle);
+    });
 }
 
+let places_been = [["37", "-76"],["33","-118"], ["34","-118"]];
+function createMarkers() {
+    let lat_guess = document.getElementById("lat").value;
+    let lng_guess = document.getElementById("lng").value;
+    let temp ="";
+    for (let i=0; i<places_been.length; i++){
+        if (JSON.stringify(places_been[i]) == JSON.stringify([lat_guess, lng_guess])){
+            temp = "Yes, I have been there! See the marker add to the Map";
+            let visited_place = new google.maps.Marker({
+                position: {lat: parseInt(lat_guess), lng: parseInt(lng_guess)},
+                map:map,
+                title: "Place I have Visited!"
+                });
+        }
+    }
+    if (temp.localeCompare("")){
+        document.getElementById("answer").innerText = temp;
+    } else {
+        document.getElementById("answer").innerText = "Sorry, no I have not been there yet. Try Again!";
+    }
+    event.preventDefault();
+}
+
+function hints() {
+  let hint = ["I've been to a city in the state that is considered the 'mother of states'", "I've been to the city the movie 'Freedom Writers' is located in", "I've been to the city nicknamed 'the Bu' "];
+  let randomHint = hint[Math.floor(Math.random() * hint.length)];
+  let hint_container = document.getElementById('hint_container');
+  hint_container.innerText = randomHint;
+}
